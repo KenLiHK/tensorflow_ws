0f1feafec2376f36a76f128c490dcccc5902e057
KenLiHK <lichiyeung@gmail.com>
2017-03-23 00:42:51 +0800

    ts13
    Add ts13

---
+++ cmf_ts13.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 00:39:04 2017
+
+Created by: Ken Li
+"""
+
+import tensorflow as tf
+
+print('*** cmf_ts13.py ***')
+print('tensorflow version:', tf.__version__)
